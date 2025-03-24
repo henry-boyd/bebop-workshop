c11a21d87f8a84a508b889a0943805004acc5122
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2025-03-23 21:34:16 -0600

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
+import vercelStatic from '@astrojs/vercel/static';
 
-import sitemap from '@astrojs/sitemap';
-
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), sitemap()],
-  adapter: cloudflare()
+  // Must be 'static' or 'hybrid'
+  output: 'static',
+  adapter: vercelStatic({
+    imageService: true,
+  }),
 });
