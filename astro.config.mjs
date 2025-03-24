09d78898c37fd7a5f1b428fd64398b9326a89ca1
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2025-03-23 18:07:21 -0600

    cloudflare updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import icon from 'astro-icon';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), sitemap()]
+  integrations: [icon(), sitemap()],
+  adapter: cloudflare()
 });
