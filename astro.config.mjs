7ad2d6fe8006e43e8eb736fdfe0166110f854c32
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2025-04-06 01:07:08 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 // Import /serverless for a Serverless SSR site
 import vercelStatic from '@astrojs/vercel/static';
 
-import netlify from '@astrojs/netlify';
-
 export default defineConfig({
   output: 'static',
-  adapter: netlify(),
+  adapter: vercelStatic(),
 });
