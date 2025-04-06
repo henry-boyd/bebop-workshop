be64984a423af54621e9cdfd464840e1360711f6
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2025-04-05 22:06:42 -0600

    vercel images issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import vercelServerless from '@astrojs/vercel/serverless';
  
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercelServerless({
     imageService: true,
     maxDuration: 8,
