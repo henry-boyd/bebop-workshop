bdaa054bb6d538bcb7d194e465f2ae672fb5a661
Henry Boyd <48170095+henry-boyd@users.noreply.github.com>
2026-03-23 12:13:28 -0600

    Uninstall vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 // Import /serverless for a Serverless SSR site
-import vercelStatic from '@astrojs/vercel/static';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercelStatic(),
 });
